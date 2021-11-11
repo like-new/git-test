337c753b207342f9530001b914ea7f1a81593882
forster <forster@qq.com>
2021-11-11 20:34:40 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author CWH
+ * @create 2021-11-11 20:29
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
