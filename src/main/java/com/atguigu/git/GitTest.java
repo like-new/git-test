b77d69007175136a600a86eb17562507367fc496
like-new <94113498+like-new@users.noreply.github.com>
2021-11-11 21:18:09 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("master git5!");
         System.out.println("hello git6!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
